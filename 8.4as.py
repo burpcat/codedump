166309977ffceb921052d8768e3d611d2fe39736
burpcat <62823253+burpcat@users.noreply.github.com>
2020-05-20 08:24:26 +0530

    Create 8.4as.py

---
+++ 8.4as.py
@@ -0,0 +1,23 @@
+8.4 Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in alphabetical order.
+You can download the sample data at http://www.py4e.com/code3/romeo.txt
+
+#Codestarts from
+
+fname = input("Enter the name of the file: ")
+
+fhan = open(fname)
+
+list1 = list()
+
+for line in fhan:
+    for word in line.split():
+        if word in list1:
+            continue
+        else:
+            list1.append(word)
+        
+list1.sort()
+#list1 =  list(dict.fromkeys(list1))
+print(list1)
+
+
